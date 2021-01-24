862c28bf07585c8d2e8ac2736bdb694e5d8724ed
Ed O'Connor <ed@oconnor.app>
2021-01-24 08:36:39 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Edward O'Connor on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
