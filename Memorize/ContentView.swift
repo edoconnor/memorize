e340cae152c2ff9864263927e85b69879463d28e
Ed O'Connor <ed@oconnor.app>
2021-01-24 09:37:05 -0500

    Initial commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView()
+            
+            }
+        }
             .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻")
+            } else {
+              RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
